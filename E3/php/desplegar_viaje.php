6afb67e9d0bd4946cffa87a268aec3de721f6853
fblazco <66034189+fblazco@users.noreply.github.com>
2025-06-16 11:38:41 -0400

    first commit

--- E3/php/desplegar_viaje.php
+++ E3/php/desplegar_viaje.php
@@ -1,63 +1,44 @@
+
 <?php
 session_start();
 if (!isset($_SESSION['usuario'])) {
     header('Location: index.php?error=Debes iniciar sesión');
     exit();
 }
-?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Detalles del Viaje</title>
-    <link rel="stylesheet" href="../css/style.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Detalles de Viaje</h1>
-        <!-- Aquí se mostrarán los detalles del viaje -->
-        <!-- La idea es que rellenen con lo solicitado en la sección 2.3 del enunciado -->
-        <!-- Apoyate con los estilos css para que se vea bonito :) -->
-        <?php
-    require_once 'utils.php';
-    $correo_usuario=$_SESSION['email'];
-    try {
-        $db = conectarBD();
+$correo_usuario= $_SESSION['email'];
 
-        $stmt = $db->prepare("
-     SELECT *
-    FROM agenda 
-    WHERE agenda.correo_usuario LIKE :email 
+
+require_once 'utils.php';
+$_SESSION['form_data']= $_POST;
+
+try {
+    $db = conectarBD();
+
+    $stmt = $db->prepare("
+    SELECT id, etiqueta
+FROM agenda
+where correo_usuario LIKE :aux 
     ");
-    $aux="%{$ciudad_llegada}%";
-    $aux2="%{$ciudad_origen}%";
-    $aux3=$cantidad_personas;
+    $aux="%{$correo_usuario}%";
+    $aux2=$cantidad_personas;
     $stmt->bindValue(':aux', $aux, PDO::PARAM_STR);
-    $stmt->bindValue(':aux2', $aux2, PDO::PARAM_STR);
-    $stmt->bindValue(':aux3', $aux3, PDO::PARAM_STR);
-    $stmt->bindValue(':fecha_inicio', $fecha_i);
-    $stmt->bindValue(':fecha_termino', $fecha_t);
     $stmt->execute();
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     if ($rows>0){
-        $_SESSION['aviones_disponibles']=$rows;
-    header('Location: show_transportes_disponibles.php');
+        $_SESSION['agendas_usuario']=$rows;
+        header('Location: show_agendas.php');
         exit();
     }else {
-        $_SESSION['error'] = "No se encontraron avionesen “{$ciudad}”";
-
+        $_SESSION['error'] = "No se encontraron hospedajes en “{$ciudad}”";
+        header('Location: desplegar_viaje.php');
+        
         exit();
     }
 
 } catch (Exception $e) {
     if ($db->inTransaction()) $db->rollBack();
     $_SESSION['error'] = 'Error al crear el viaje';
-    header('Location: index.php');
+    header('Location: main.php');
     exit();
 }
-        
-
-        ?>
-    </div>
-</body>
-</html>
+?>
